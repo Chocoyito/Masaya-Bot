d3330045176085b7f8a49f33bae29665f5cc2834
Chocoyito <83928147+Chocoyito@users.noreply.github.com>
2021-09-28 02:37:04 +0000

    auto-format

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 import json
 from discord.ext import commands
 import asyncio
+
 load_dotenv()
 import tracemalloc
 import youtube_dl
@@ -12,51 +13,48 @@ import youtube_dl
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="$",intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 tracemalloc.start()
 
+
 def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return (quote)
 
 
-    
-@bot.command(
-	help="Es para ver si el bot recibe ordenes",
-	brief="El bot devuelve Pong! si todo funciona bien."
-)
+@bot.command(help="Es para ver si el bot recibe ordenes",
+             brief="El bot devuelve Pong! si todo funciona bien.")
 async def ping(ctx):
-  await ctx.channel.send('Pong!')
+    await ctx.channel.send('Pong!')
+
 
 @bot.command(
-	# ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
-	help="$say (argumento)",
-	# ADDS THIS VALUE TO THE $HELP MESSAGE.
-	brief="Repite lo que escribiste"
-)
+    # ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
+    help="$say (argumento)",
+    # ADDS THIS VALUE TO THE $HELP MESSAGE.
+    brief="Repite lo que escribiste")
 async def say(ctx, *args):
-	response = ""
+    response = ""
 
-	# Itera el ciclo
-	for arg in args:
-		response = response + " " + arg
+    # Itera el ciclo
+    for arg in args:
+        response = response + " " + arg
 
-	# manda un mensaje al canal usando el contexto de objeto
-	await ctx.channel.send(response)
-  
-@bot.command(
-	help="Documentacion de C modo grafico",
-	brief="Lanza documentacion de C grafico"
-)
+    # manda un mensaje al canal usando el contexto de objeto
+    await ctx.channel.send(response)
+
+
+@bot.command(help="Documentacion de C modo grafico",
+             brief="Lanza documentacion de C grafico")
 async def docgrafico(ctx):
-  await ctx.channel.send('Documentancion de C grafico')
-  await ctx.channel.send(file=discord.File('Documentacion/Doc.pdf'))
-  await ctx.channel.send(file=discord.File('Documentacion/EJEM1.c'))
+    await ctx.channel.send('Documentancion de C grafico')
+    await ctx.channel.send(file=discord.File('Documentacion/Doc.pdf'))
+    await ctx.channel.send(file=discord.File('Documentacion/EJEM1.c'))
+
 
 youtube_dl.utils.bug_reports_message = lambda: ""
-
 
 ytdl_format_options = {
     "format": "bestaudio/best",
@@ -69,7 +67,8 @@ ytdl_format_options = {
     "quiet": True,
     "no_warnings": True,
     "default_search": "auto",
-    "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
+    "source_address":
+    "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
 ffmpeg_options = {"options": "-vn"}
@@ -90,15 +89,15 @@ class YTDLSource(discord.PCMVolumeTransformer):
     async def from_url(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(
-            None, lambda: ytdl.extract_info(url, download=not stream)
-        )
+            None, lambda: ytdl.extract_info(url, download=not stream))
 
         if "entries" in data:
             # take first item from a playlist
             data = data["entries"][0]
 
         filename = data["url"] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options),
+                   data=data)
 
 
 class Music(commands.Cog):
@@ -119,9 +118,9 @@ class Music(commands.Cog):
         """Plays a file from the local filesystem"""
 
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(
-            source, after=lambda e: print(f"Player error: {e}") if e else None
-        )
+        ctx.voice_client.play(source,
+                              after=lambda e: print(f"Player error: {e}")
+                              if e else None)
 
         await ctx.send(f"Now playing: {query}")
 
@@ -131,9 +130,9 @@ class Music(commands.Cog):
 
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(
-                player, after=lambda e: print(f"Player error: {e}") if e else None
-            )
+            ctx.voice_client.play(player,
+                                  after=lambda e: print(f"Player error: {e}")
+                                  if e else None)
 
         await ctx.send(f"Now playing: {player.title}")
 
@@ -142,10 +141,12 @@ class Music(commands.Cog):
         """Streams from a url (same as yt, but doesn't predownload)"""
 
         async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            ctx.voice_client.play(
-                player, after=lambda e: print(f"Player error: {e}") if e else None
-            )
+            player = await YTDLSource.from_url(url,
+                                               loop=self.bot.loop,
+                                               stream=True)
+            ctx.voice_client.play(player,
+                                  after=lambda e: print(f"Player error: {e}")
+                                  if e else None)
 
         await ctx.send(f"Now playing: {player.title}")
 
@@ -174,7 +175,8 @@ class Music(commands.Cog):
                 await ctx.author.voice.channel.connect()
             else:
                 await ctx.send("You are not connected to a voice channel.")
-                raise commands.CommandError("Author not connected to a voice channel.")
+                raise commands.CommandError(
+                    "Author not connected to a voice channel.")
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
 
@@ -184,107 +186,114 @@ bot = commands.Bot(
     description="Relatively simple music bot example",
 )
 
+
 @bot.event
 async def status_task():
     while True:
-        await bot.change_presence(activity=discord.Game("Lovo lindo"), status=discord.Status.idle)
-        await asyncio.sleep(3) # cambia luego de x segundos
-        await bot.change_presence(activity=discord.Game("Paja"), status=discord.Status.idle)
+        await bot.change_presence(activity=discord.Game("Lovo lindo"),
+                                  status=discord.Status.idle)
+        await asyncio.sleep(3)  # cambia luego de x segundos
+        await bot.change_presence(activity=discord.Game("Paja"),
+                                  status=discord.Status.idle)
         await asyncio.sleep(3)
-        await bot.change_presence(activity=discord.Game("Pornhub"), status=discord.Status.idle)
+        await bot.change_presence(activity=discord.Game("Pornhub"),
+                                  status=discord.Status.idle)
         await asyncio.sleep(3)
-        await bot.change_presence(activity=discord.Streaming(name='TurboC es completamente basura', url='https://www.twitch.tv/tugfammg'))
+        await bot.change_presence(
+            activity=discord.Streaming(name='TurboC es completamente basura',
+                                       url='https://www.twitch.tv/tugfammg'))
         await asyncio.sleep(3)
-        await bot.change_presence(activity=discord.Streaming (name='Roberto lindo', url='https://www.twitch.tv/tugfammg'))
+        await bot.change_presence(activity=discord.Streaming(
+            name='Roberto lindo', url='https://www.twitch.tv/tugfammg'))
     await bot.process_commands(status_task)
 
 
 @bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'
-  .format(bot))
-  bot.loop.create_task(status_task()) # Create loop/task
-
+    print('We have logged in as {0.user}'.format(bot))
+    bot.loop.create_task(status_task())  # Create loop/task
 
 
 @bot.event
 async def on_message(message):
-   
-  if message.author == bot.user:
-    return
 
-  if message.content.lower()=="frase":
-    quote=get_quote() 
-    await message.channel.send(quote)
+    if message.author == bot.user:
+        return
 
-  if message.content.lower()=="yon":
-    await message.channel.send('Mi hombre')
-    await message.channel.send(file=discord.File('resources/YON.jpg'))
+    if message.content.lower() == "frase":
+        quote = get_quote()
+        await message.channel.send(quote)
 
-  if message.content.lower()=="sexo":
-    await message.channel.send('Lo unico que no vas a heredar de tus padres.')
+    if message.content.lower() == "yon":
+        await message.channel.send('Mi hombre')
+        await message.channel.send(file=discord.File('resources/YON.jpg'))
 
-  if message.content.lower()=="leonel":
-    await message.channel.send(file=discord.File('resources/esposa.jpg'))
+    if message.content.lower() == "sexo":
+        await message.channel.send(
+            'Lo unico que no vas a heredar de tus padres.')
 
-  if message.content.lower() == "te quiero mucho":
-    await message.channel.send(file=discord.File('resources/mp4.gif'))
+    if message.content.lower() == "leonel":
+        await message.channel.send(file=discord.File('resources/esposa.jpg'))
 
-  if message.content.lower() == "passchipeada":
-    await message.channel.send(file=discord.File('resources/leonel.c'))
+    if message.content.lower() == "te quiero mucho":
+        await message.channel.send(file=discord.File('resources/mp4.gif'))
+        await message.add_reaction("🥰")
+    if message.content.lower() == "passchipeada":
+        await message.channel.send(file=discord.File('resources/leonel.c'))
 
-  if message.content.lower()=="infravalorada" :
-    await message.channel.send(file=discord.File('resources/yichu.jpg'))
+    if message.content.lower() == "infravalorada":
+        await message.channel.send(file=discord.File('resources/yichu.jpg'))
 
-  if message.content.lower()=="¿quien soy?":
-      await message.channel.send('Vos sos @{}'.format(message.author.name))
+    if message.content.lower() == "¿quien soy?":
+        await message.channel.send('Vos sos @{}'.format(message.author.name))
 
-  if message.content.lower()=="elpepe":
-    await message.channel.send(file=discord.File('resources/elpepe.mp4'))
+    if message.content.lower() == "elpepe":
+        await message.channel.send(file=discord.File('resources/elpepe.mp4'))
 
-  if message.content.lower()=="crudninja":
-    await message.channel.send(file=discord.File('TAREAS/pipe.c'))
-    await message.channel.send(file=discord.File('TAREAS/tupapa.h'))
-    await message.channel.send(file=discord.File('TAREAS/CRUD.rar'))
+    if message.content.lower() == "crudninja":
+        await message.channel.send(file=discord.File('TAREAS/pipe.c'))
+        await message.channel.send(file=discord.File('TAREAS/tupapa.h'))
+        await message.channel.send(file=discord.File('TAREAS/CRUD.rar'))
 
-  if message.content.lower()=="grevinpaja":
-    await message.channel.send(file=discord.File('TAREAS/Munecos.C'))
-    await message.channel.send(file=discord.File('TAREAS/tupapa.h'))
-    await message.channel.send(file=discord.File('TAREAS/MARIOBROS.h'))
+    if message.content.lower() == "grevinpaja":
+        await message.channel.send(file=discord.File('TAREAS/Munecos.C'))
+        await message.channel.send(file=discord.File('TAREAS/tupapa.h'))
+        await message.channel.send(file=discord.File('TAREAS/MARIOBROS.h'))
 
-  if message.content.lower()=="lovo":
-    await message.channel.send(file=discord.File('resources/lovo.mp4'))
+    if message.content.lower() == "lovo":
+        await message.channel.send(file=discord.File('resources/lovo.mp4'))
 
-  if message.content.lower()=="*dormir*":
-    await message.channel.send('¿Vamos a dormir U//U?')
-    await message.channel.send(file=discord.File('waifu/dormir.png'))
-  
-  if message.content.lower()=="*me siento triste*":
-    await message.channel.send('¿Que tenés bb? contame u.u')
-    await message.channel.send(file=discord.File('waifu/triste.gif'))
+    if message.content.lower() == "*dormir*":
+        await message.channel.send('¿Vamos a dormir (。U⁄ ⁄ω⁄ ⁄ U。)??')
+        await message.add_reaction("😳")
+        await message.channel.send(file=discord.File('waifu/dormir.png'))
 
-  if message.content.lower()=="*te cuento*":
-    await message.channel.send('contame')
-    await message.channel.send(file=discord.File('waifu/contame.gif'))
+    if message.content.lower() == "*me siento triste*":
+        await message.channel.send('¿Que tenés bb? contame u.u')
+        await message.channel.send(file=discord.File('waifu/triste.gif'))
 
-  if message.content.lower().startswith("lo que pasa es que"):
-    await message.channel.send('¿Y como te hace sentir eso?')
-    await message.channel.send(file=discord.File('waifu/feeling.gif'))
+    if message.content.lower() == "*te cuento*":
+        await message.channel.send('contame')
+        await message.channel.send(file=discord.File('waifu/contame.gif'))
 
-  if message.content.lower()=="logouni3d":
-    await message.channel.send(file=discord.File('resources/cuento.c'))
+    if message.content.lower().startswith("lo que pasa es que"):
+        await message.channel.send('¿Y como te hace sentir eso?')
+        await message.channel.send(file=discord.File('waifu/feeling.gif'))
 
-  await bot.process_commands(message)
+    if message.content.lower() == "logouni3d":
+        await message.channel.send(file=discord.File('resources/cuento.c'))
 
-
+    await bot.process_commands(message)
 
 
 async def setup():
     await bot.wait_until_ready()
 
+
 async def on_message_delete(self, message):
-        msg = f"{message.author} has deleted the message: {message.content}"
-        await message.channel.send(msg)
+    msg = f"{message.author} has deleted the message: {message.content}"
+    await message.channel.send(msg)
+
 
 @bot.command()
 async def repeat(ctx, times: int, content="repeating..."):
@@ -292,16 +301,14 @@ async def repeat(ctx, times: int, content="repeating..."):
     for i in range(times):
         await ctx.send(content)
 
+
 @bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f"{member.name} Entro una puta en {member.joined_at}")
 
 
-
-
 bot.add_cog(Music(bot))
 bot.loop.create_task(setup())
-
 
 bot.run(os.getenv('TOKEN'))
